d83e1230ddbfcc307c160e5d8cb59b2e84d7a8f0
Habibullah Hamidi <h.hamidi>
2025-02-27 10:24:03 +0430

    post method added

--- src/app.js
+++ src/app.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
 
-// Log all requests
-app.use((req, res, next) => {
-   console.log(`Request received: ${req.method} ${req.url}`);
-   next();
-});
-
 // Define specific routes first
-app.use("/hello", (req, res) => {
-   res.send('Hello from the server');
+app.get("/test", (req, res) => {
+   res.send('Test  from the server');
 });
 
-app.use("/demo", (req, res) => {
-   res.send('Demo');
+app.post("/user", (req, res) => {
+   res.send("post method call");
 });
-
-// Define the catch-all route last
-// app.use("/", (req, res) => {
-//    res.send('from dashboard');
-// });
+app.delete("/demo", (req, res) => {
+   res.send("de method call");
+});
+app.put("/edit", (req, res) => {
+   res.send("update method call");
+});
 
 app.listen(8888, () => {
    console.log('Server is successfully listening on port 8888...');
