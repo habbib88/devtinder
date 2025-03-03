617fbf7efc11b6a766a5f6de1c7f018b433d2eb6
Habibullah Hamidi <h.hamidi>
2025-03-03 11:27:55 +0430

    get user profile

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const User = require("./models/user");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/database");
+const jwt = require("jsonwebtoken");
 app.use(express.json());
+app.use(cookieParser());
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -10,6 +13,38 @@ app.post("/signup", async (req, res) => {
     res.send('user added');
   } catch (err) {
     res.status(400).send("Error saving the user" + err.message);
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    if (user.password !== password) {
+      return res.status(400).send("Invalid credentials");
+    }
+    if (user.password === password) {
+      const token = await jwt.sign({_id:user._id},"DEV@TINDER88");
+      res.cookie("token", token);
+      res.send("Login successful");
+    }
+  } catch (err) {
+    res.status(500).send("Error logging in: " + err.message);
+  }
+});
+app.get("/profile", async (req, res) => {
+  try {
+    const cookie = req.cookies; //get cookie
+    const {token} = cookie; 
+    //validate the token  and decode it
+    const decodedMessage = await jwt.verify(token, "DEV@TINDER88");
+    const { _id } = decodedMessage;
+    const user = await User.findById(_id);
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Error retrieving the profile: " + err.message);
   }
 });
 app.get("/user", async (req, res) => {
--- src/models/user.js
+++ src/models/user.js
@@ -20,5 +20,20 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String
     },
+    about: {
+        type: String,
+        default: "No information provided"
+    },
+    skills: {
+        type: [String],
+        default:['reactjs','js','next js','node js']
+    },
+    photoUrl: {
+        type: String,
+        default:"‪C://Users/hamidi/Pictures/9.jpg"
+    },
+   
+}, {
+    timestamps: true,
 });
 module.exports = mongoose.model("User",userSchema);
